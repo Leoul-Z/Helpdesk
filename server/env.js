c716d394b6912ff85f40a1c263521a66c4999337
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-09-21 12:19:24 +0300

    fix env.js

--- server/env.js
+++ server/env.js
@@ -4,7 +4,7 @@ const env = {
     PORT: Number(process.env.PORT),
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-    clientUrl: process.env.CLIENT_URL,
+    CLIENT_URL: process.env.CLIENT_URL,
     DATABASE_URL: process.env.DATABASE_URL,
 }
 
